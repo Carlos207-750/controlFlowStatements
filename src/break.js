8452a02705def487aeab926f6593d43c35446bd5
Carlos207-750 <pl30qfr@eduvaud.ch>
2024-09-13 14:43:45 +0200

    new

--- src/break.js
+++ src/break.js
@@ -1,8 +1,27 @@
 const numbers = [];
+let operations = 0;
+let avoidedOperations = 0;
+let indexOfNumberOver50 = -1;
+
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
+
 for (let i = 0; i < numbers.length; i++) {
+    operations++;
     console.log(numbers[i]);
+    if (numbers[i] > 50) {
+        indexOfNumberOver50 = i;
+        break;
+    }
 }
+
+
+avoidedOperations = numbers.length - (indexOfNumberOver50 + 1);
+
+
+console.log("Opérations réalisées: " + operations);
+console.log("Opérations évitées: " + avoidedOperations);
+console.log("Premier nombre supérieur à 50 trouvé à l'indice : " + indexOfNumberOver50);
